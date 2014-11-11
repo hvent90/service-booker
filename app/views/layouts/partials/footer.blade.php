ba96cc5884a3e18775c82028d61f961e13b90bf4
Henry Ventura <hvent90@gmail.com>
2014-11-11 13:01:58 -0500

    Woa

--- app/views/layouts/partials/footer.blade.php
+++ app/views/layouts/partials/footer.blade.php
@@ -8,4 +8,3 @@
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
 <script src="https://code.jquery.com/color/jquery.color-2.1.2.min.js"></script>
-<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
