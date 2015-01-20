e8d33b62e89870f32d8cd00b0936c2d87f236c54
miscbits <willow6786@gmail.com>
2015-01-20 16:55:03 -0500

    fixed login form css

--- app/views/layouts/partials/nav/login.blade.php
+++ app/views/layouts/partials/nav/login.blade.php
@@ -1,6 +1,6 @@
 {{ Form::open(['route' => 'advisors.login']) }}
 
-    <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
+    <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }} login-form">
         {{ Form::label('email', 'Email Address') }}
         {{ Form::text('email', null, ['class' => 'form-control', 'placeholer' => 'Email Address']) }}
         {{ $errors->first('email', '<p class="help-block">:message</p>') }}
@@ -11,7 +11,7 @@
     <div class="input-group">
         {{ Form::password('password', ['class' => 'form-control']) }}
         <span class="input-group-btn nav-login-submit">
-            {{ Form::submit('Go!', ['class' => 'btn btn-primary'])}}
+            {{ Form::submit('Go!', ['class' => 'btn btn-primary btn-login'])}}
         </span>
     </div>
 
