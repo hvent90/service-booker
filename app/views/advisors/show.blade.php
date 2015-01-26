b9268997e63044f235f46dab64268fd47ca15a6a
Henry Ventura <hvent90@gmail.com>
2015-01-26 13:18:28 -0500

    header on advisor show page now mimicks index

--- app/views/advisors/show.blade.php
+++ app/views/advisors/show.blade.php
@@ -4,9 +4,11 @@
 <div class="container">
 	<div class="row">
 		<div class="heading-text col-sm-12">
-			<a href="http://walnutstlabs.com"><img src="/img/wsllogo.jpg" class="img-responsive"></a>
-			<h2 style="display: inline-block;">Advance Your Idea. Faster.</h2>
-			<h4>Walnut St. Labs Office Hours is a joint project between Walnut St. Labs and the Chester County Economic Development Council that offers innovators the opportunity to gain expert advice from industry and domain experts from Chester County and Southeastern PA.</h4>
+			<a href="http://walnutstlabs.com"><img src="/img/wsllogo.jpg" ></a>
+			<a href="http://www.i2npa.org/"><img src="/img/i2n.png" ></a>
+			<br />
+			<h2>Advance Your Idea. Faster.</h2>
+			<h4>Office Hours is a joint project between <span id="wsl-tag"><a href="http://walnutstlabs.com">Walnut St. Labs</a></span> and the <span id="i2n-tag"><a href="http://www.i2npa.org/">Ideas x Innovation Network</a></span> that offers innovators the opportunity to gain expert advice from industry and domain experts from Chester County and Southeastern PA.</h4>
 		</div>
 	</div>
 	<div class="row col-sm-12 expertise-group-listing">
