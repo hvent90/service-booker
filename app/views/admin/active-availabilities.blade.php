918ccf2893b3d7f6991cf91c818d8050f2ec5657
Henry Ventura <hvent90@gmail.com>
2015-01-27 14:30:52 -0500

    added conditional for activity monitor to fix when there are no requests for availabilities

--- app/views/admin/active-availabilities.blade.php
+++ app/views/admin/active-availabilities.blade.php
@@ -3,30 +3,32 @@
 @section('content')
 
 <div class="row">
-@foreach($availabilities as $availability)
-	<div class="advisor-header col-sm-3" style="border: 1px solid #999; padding: 10px; margin: 5px">
-	<div style="padding: 5px">
-		@if($availability->advisor_profile_img) 
-			<img src="{{ $availability->advisor_profile_img }}" class="img-responsive">
+@if($availabilities)
+	@foreach($availabilities as $availability)
+		<div class="advisor-header col-sm-3" style="border: 1px solid #999; padding: 10px; margin: 5px">
+		<div style="padding: 5px">
+			@if($availability->advisor_profile_img) 
+				<img src="{{ $availability->advisor_profile_img }}" class="img-responsive">
+			@endif
+			{{ $availability->advisor_first_name }} {{ $availability->advisor_last_name }} <br />
+			{{ $availability->advisor_email }} <br />
+		</div>
+		<hr>
+		{{ $availability->requestee_name }} <br />
+		{{ $availability->requestee_email }} <br />
+		{{ $availability->requestee_phone }} <br />
+		{{ $availability->requestee_notes }} <br />
+		{{ $availability->time }} <br />
+		{{ $availability->date }} <br />
+		{{ $availability->location }} <br />
+		@if($availability->status == 0)
+			{{ $availability->advisor_first_name }} {{ $availability->advisor_last_name }} has not yet confirmed this request.
+		@else
+			{{ $availability->advisor_first_name }} {{ $availability->advisor_last_name }} has CONFIRMED this request.
 		@endif
-		{{ $availability->advisor_first_name }} {{ $availability->advisor_last_name }} <br />
-		{{ $availability->advisor_email }} <br />
-	</div>
-	<hr>
-	{{ $availability->requestee_name }} <br />
-	{{ $availability->requestee_email }} <br />
-	{{ $availability->requestee_phone }} <br />
-	{{ $availability->requestee_notes }} <br />
-	{{ $availability->time }} <br />
-	{{ $availability->date }} <br />
-	{{ $availability->location }} <br />
-	@if($availability->status == 0)
-		{{ $availability->advisor_first_name }} {{ $availability->advisor_last_name }} has not yet confirmed this request.
-	@else
-		{{ $availability->advisor_first_name }} {{ $availability->advisor_last_name }} has CONFIRMED this request.
-	@endif
-	</div>
-@endforeach
+		</div>
+	@endforeach
+@endif
 </div>
 
 @stop
