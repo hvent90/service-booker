65917a0adf65a74a2a22c92d70a13536740940d3
Henry Ventura <hvent90@gmail.com>
2015-01-27 10:51:17 -0500

    jk

--- app/models/Advisor.php
+++ app/models/Advisor.php
@@ -127,12 +127,12 @@ class Advisor extends \Eloquent implements UserInterface, RemindableInterface {
 		$expOfAdv = $expOfAdv->modelKeys();
 
 		$expertise = DB::table('expertise')
-							->leftJoin('expertisegroup_expertise',
-										'expertise.id', '=',
-										'expertisegroup_expertise.expertise_id')
-							->whereIn('expertisegroup_expertise.expertise_id', $expOfAdv)
-							->where('expertisegroup_expertise.expertise_group_id', $expGroupId)
-							->get();
+			->leftJoin('expertisegroup_expertise',
+						'expertise.id', '=',
+						'expertisegroup_expertise.expertise_id')
+			->whereIn('expertisegroup_expertise.expertise_id', $expOfAdv)
+			->where('expertisegroup_expertise.expertise_group_id', $expGroupId)
+			->get();
 
 		// if($this->first_name == 'Terry'){
 		// 	dd(
